7cc7ef262e9395eedfc5d77f35432d2b91fb5993
Eric Zanchi <emzanchi@gmail.com>
2017-03-20 12:33:07 -0500

    Updated build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const nodemon = require('gulp-nodemon');
-const sass = require('gulp-sass');
 const path = require('path');
 const pump = require('pump');
 const config = require('./webpack.config.js');
@@ -14,31 +13,14 @@ gulp.task('webpack', function(cb){
   cb();
 });
 
-gulp.task('scss', function(cb){
-  pump([
-    gulp.src('src/scss/main.scss'),
-    sass(),
-    gulp.dest('./public/css')
-  ],cb);
-});
-
-gulp.task('default', ['webpack', 'scss'], function(cb){
+gulp.task('default', ['webpack'], function(cb){
   return nodemon({
     delay: 10,
     ignore: ['public/**/*.*'],
     scripts: {
       start: 'node app.js'
     },
-    tasks(changedFiles){
-      const tasks = [];
-      for(let file of changedFiles){
-        if(path.extname(file) === '.js' || path.extname(file) === '.vue')
-          tasks.push('webpack');
-        if(path.extname(file) === '.scss')
-          tasks.push('scss');
-      }
-      return tasks;
-    },
+    tasks: ['webpack'],
     watch: '**/*.*'
   });
 });
